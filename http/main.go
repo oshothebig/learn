a3ecb5241110530f1b00ec3fd4542e27de2cc69f
Sho SHIMIZU <sho.shimizu@gmail.com>
2022-01-18 18:08:19 +0900

    Extract method handling POST method

--- http/main.go
+++ http/main.go
@@ -15,10 +15,14 @@ type Handler struct{}
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		fmt.Fprintln(w, "Hello")
+		h.Post(w, r)
 	default:
 		h.unsupportedMethod(w, r)
 	}
+}
+
+func (h *Handler) Post(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello")
 }
 
 func (h *Handler) unsupportedMethod(w http.ResponseWriter, r *http.Request) {
