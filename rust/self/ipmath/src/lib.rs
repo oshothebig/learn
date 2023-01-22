b3541a44d8887681301e617023f31a78fc17166b
Sho SHIMIZU <sho.shimizu@gmail.com>
2023-01-22 10:38:28 +0900

    Add test for add_ipv6()
    Unused imports warning must be resolved later

--- rust/self/ipmath/src/lib.rs
+++ rust/self/ipmath/src/lib.rs
@@ -1,4 +1,5 @@
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
+use std::str::FromStr;
 
 pub fn add_ipv4(lhs: Ipv4Addr, rhs: u32) -> Ipv4Addr {
     let octets = lhs.octets();
@@ -31,6 +32,13 @@ pub fn add_ipv6(lhs: Ipv6Addr, rhs: u128) -> Ipv6Addr {
     let octets = lhs.octets();
     let num_repr = u128::from_be_bytes(octets) + rhs;
     Ipv6Addr::from(num_repr)
+}
+
+#[test]
+fn test_add_ipv6_one() {
+    let lhs: Ipv6Addr = "::1".parse().unwrap();
+    let actual = add_ipv6(lhs, 1);
+    assert_eq!(actual, "::2".parse::<Ipv6Addr>().unwrap());
 }
 
 pub fn add(lhs: IpAddr, rhs: u128) -> IpAddr {
