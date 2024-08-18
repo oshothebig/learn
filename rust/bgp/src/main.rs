58465eb8f4bf9e928685addb1b63a4744ca732f2
Sho SHIMIZU <sho.shimizu@gmail.com>
2024-08-19 08:30:54 +0900

    Support multiple serial incoming connections

--- rust/bgp/src/main.rs
+++ rust/bgp/src/main.rs
@@ -1,12 +1,24 @@
 use std::io::Read;
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:179").unwrap();
     println!("Server listening on port 179");
 
-    let (mut stream, _) = listener.accept().unwrap();
-    println!("Incoming connection: {}", stream.peer_addr().unwrap());
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handle_client(stream),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let addr = match stream.peer_addr() {
+        Ok(peer_addr) => peer_addr.to_string(),
+        Err(e) => e.to_string(),
+    };
+    println!("Incoming connection: {}", addr);
 
     let mut buffer = [0u8; 1024 * 4];
     loop {
